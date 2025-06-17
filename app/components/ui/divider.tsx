bcc8b5cefcf02b5b5fac5b0dc7ee18a7e80a7d5f
Lucas Tonussi <lptonussi@gmail.com>
2025-06-17 11:24:58 -0300

    Add divider and main slogan

---
+++ app/components/ui/divider.tsx
@@ -0,0 +1,3 @@
+export function Divider() {
+  return <div className="w-1/2 h-px bg-gray-200 dark:bg-gray-800 mx-auto" />;
+}
--- app/components/welcome/welcome.tsx
+++ app/components/welcome/welcome.tsx
@@ -1,11 +1,13 @@
-import { Layout } from "../ui/layout";
+import { MainSlogan } from "../ui/main-slogan";
 import { Navbar } from "../ui/navbar";
 
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <Navbar />
-      <Layout />
-    </main>
+    <div className="w-screen h-screen flex items-center justify-center relative overflow-hidden">
+      <main className="flex items-center justify-center pt-16 pb-4 relative z-10">
+        <Navbar />
+        <MainSlogan />
+      </main>
+    </div>
   );
 }
