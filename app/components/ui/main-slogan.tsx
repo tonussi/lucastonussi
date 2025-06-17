a350ebf5a8e9abfd92bbbe872a6060f7c172d3cf
Lucas Tonussi <lptonussi@gmail.com>
2025-06-17 11:25:57 -0300

    Update main slogan text for clarity and improved messaging

--- app/components/ui/main-slogan.tsx
+++ app/components/ui/main-slogan.tsx
@@ -13,7 +13,7 @@ export function MainSlogan() {
           </div>
           <div className="flex items-center justify-center">
             <h1 className="bg-[radial-gradient(138.06%_1036.51%_at_95.25%_-2.54%,_#7ED4FD_14.06%,#709DF7_51.02%,#4D78EF_79.09%)] bg-clip-text text-5xl leading-[1.2] tracking-tighter text-transparent sm:text-center sm:text-[4rem] sm:leading-[4.75rem] lg:text-left">
-              come to life with humanized Whatsapp Chatbot
+              come to life with humanized Whatsapp conversations
             </h1>
           </div>
         </div>
