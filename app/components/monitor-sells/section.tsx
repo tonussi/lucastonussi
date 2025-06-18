33cb0abba1be8c2ce8ca2e9be8225125d74e22fa
Lucas Tonussi <lptonussi@gmail.com>
2025-06-18 12:18:30 -0300

    Update MonitorSellsSection to use consistent background color; replace light background class with a standard gray class for improved styling.

--- app/components/monitor-sells/section.tsx
+++ app/components/monitor-sells/section.tsx
@@ -3,7 +3,7 @@ import { MonitorSellsDescription } from './description'
 
 export function MonitorSellsSection() {
   return (
-    <div className="grid xs:grid-rows-2 lg:grid-cols-2 content-center dark:bg-slate-800 light:bg-gray-50 p-30 gap-20">
+    <div className="grid xs:grid-rows-2 lg:grid-cols-2 content-center dark:bg-slate-800 bg-gray-50 p-30 gap-20">
       <MonitorSellsDescription />
       <div className="lg:w-1/2 xs:w-full justify-self-center">
         <MonitorSellsCard />
