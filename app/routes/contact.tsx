07e6b1ee1cc09401647a058930f41ffdb961b856
Lucas Tonussi <lptonussi@gmail.com>
2025-06-21 11:14:43 -0300

    Refactor contact page to improve clarity by replacing dynamic translations with static content; enhance layout for better presentation of contact information.

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next'
 import type { Route } from './+types/home'
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: t('contact.title') }, { name: 'description', content: t('contact.title') }]
+  return [{ title: 'Contact' }, { name: 'description', content: 'Contact' }]
 }
 
 export default function Contact() {
@@ -10,9 +10,10 @@ export default function Contact() {
 
   return (
     <div className="h-full" style={{ height: '20vh' }}>
-      <div className="flex flex-col items-center justify-center">
-        <p>{t('contact.title')}</p>
-        <a href="mailto:lptonussi@gmail.com">lptonussi@gmail.com</a>
+      <div className="flex flex-col items-center justify-center p-10">
+        <p>
+          {t('contact.title')} <a href="mailto:lptonussi@gmail.com">lptonussi@gmail.com</a>
+        </p>
       </div>
     </div>
   )
