1725221c36eb59cf2732fbaca5a13c7203391fab
Lucas Tonussi <lptonussi@gmail.com>
2025-06-25 12:41:10 -0300

    Update MonitorSellsSection component: change background to a gradient for improved visual appeal and user experience.

--- app/components/monitor-sells/section.tsx
+++ app/components/monitor-sells/section.tsx
@@ -4,7 +4,7 @@ import { MonitorSellsDescription } from './description'
 
 export function MonitorSellsSection() {
   return (
-    <div className="grid xs:grid-rows-2 lg:grid-cols-2 content-center dark:bg-slate-800 bg-gray-50 p-30 gap-20">
+    <div className="grid xs:grid-rows-2 lg:grid-cols-2 content-center bg-gradient-to-r from-blue-50 to-gray-100 p-30 gap-20">
       <MonitorSellsDescription />
       <div className="lg:w-3/4 lg:h-2/4 justify-self-center">
         <MonitorSellsCard />
