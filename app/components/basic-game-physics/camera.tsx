768a1ad2ed59ef0ee460f75947454e3c60b3085e
Lucas Tonussi <lptonussi@gmail.com>
2025-06-29 14:09:44 -0300

    Refactor CameraHelper in BasicGamePhysics: update camera initialization by adding updateMatrixWorld call and commenting out default parameters for clarity and future reference.

--- app/components/basic-game-physics/camera.tsx
+++ app/components/basic-game-physics/camera.tsx
@@ -5,9 +5,15 @@ import * as THREE from 'three'
 extend(THREE as any)
 
 function CameraHelper({ refCamera }: { refCamera: RefObject<THREE.PerspectiveCamera> }) {
+  // fov: 60,
+  // near: 1,
+  // far: 1000,
+  // rotation: [-0.3, 0, 0],
+  // position: [0, 10, 10],
   const camera = new THREE.PerspectiveCamera(60, 1, 1, 3)
   camera.position.set(0, 10, 10)
   camera.rotation.set(-0.3, 0, 0)
+  camera.updateMatrixWorld()
   return (
     <group ref={refCamera}>
       <cameraHelper name="camera" args={[camera]} />
