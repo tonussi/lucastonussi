471b9329b57add12783a0dc6d83754ee386ec8a5
Lucas Tonussi <lptonussi@gmail.com>
2025-07-08 11:32:40 -0300

    Add

--- app/routes/canvas.tsx
+++ app/routes/canvas.tsx
@@ -14,7 +14,8 @@ export default function BasicGamePhysics() {
   const refScene = useRef<THREE.Scene>(null!)
   const [torusRotation, setTorusRotation] = useState(0)
   const [torusPositions, setTorusPositions] = useState<{ x: number; z: number }[]>([])
-  const { color } = useControls('Fog', { color: '#000' })
+  const { color } = useControls('Fog', { color: '#ad6df4' })
+  const [bgColor, setBgColor] = useState('rgba(173,109,244,0.5)')
 
   useEffect(() => {
     const positions = Array.from({ length: 10 }, () => ({
@@ -32,11 +33,22 @@ export default function BasicGamePhysics() {
     return () => clearInterval(interval)
   }, [torusRotation])
 
+  const hexToRgba = (hex: string) => {
+    console.log('hex', hex)
+    if (!hex) return 'rgba(173,109,244,0.5)'
+    const [r, g, b] = hex.match(/\w{2}/g)!.map((x) => parseInt(x, 16))
+    return `rgba(${r},${g},${b},0.5)`
+  }
+
   return (
     <>
-      <div className="top-0 -z-10 h-full w-full bg-white">
-        <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
-      </div>
+      {bgColor && (
+        <div className="top-0 -z-10 h-full w-full bg-white">
+          <div
+            className={`absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[${bgColor}] opacity-50 blur-[80px]`}
+          />
+        </div>
+      )}
       <Canvas
         fallback={'Sorry no WebGL supported!'}
         style={{
@@ -51,25 +63,36 @@ export default function BasicGamePhysics() {
         <Suspense fallback={null}>
           <Physics>
             <scene ref={refScene}>
-              <ambientLight intensity={0.1} />
-              <directionalLight color="red" position={[0, 0, 5]} />
+              <ambientLight intensity={0.5} />
+              <spotLight name="spotlight" position={[0, 10, 0]} intensity={20} />
+              <directionalLight
+                position={[10, 10, 10]}
+                shadow-mapSize-width={1024}
+                shadow-mapSize-height={1024}
+                intensity={11.5}
+              />
               {torusPositions.map((position, i) => (
                 <group key={i}>
-                  <mesh castShadow receiveShadow>
-                    <group>
+                  <group>
+                    <mesh castShadow>
                       <Torus
-                        castShadow
                         args={[0.5, 0.09, 64, 64]}
                         position={[position.x, 1.1, position.z]}
                         rotation={[0, torusRotation * (i % 2 === 0 ? 1 : -1), 0]}
                       >
-                        <meshStandardMaterial />
+                        <meshStandardMaterial
+                          color={color}
+                          roughness={0.5}
+                          metalness={0.5}
+                          opacity={0.5}
+                          transparent
+                          wireframe
+                        />
                       </Torus>
                       {/* Add tiny segments */}
                       {Array.from({ length: 12 }, (_, j) => (
                         <mesh
                           key={j}
-                          castShadow
                           position={[
                             position.x + Math.random() * 0.2 - 0.1,
                             1 + Math.random() * 0.2 - 0.1,
@@ -89,29 +112,30 @@ export default function BasicGamePhysics() {
                           />
                         </mesh>
                       ))}
-                    </group>
-                  </mesh>
-                  <mesh castShadow receiveShadow>
+                    </mesh>
+                  </group>
+                  <mesh castShadow>
                     <Torus
-                      castShadow
                       args={[0.4, 0.09, 64, 64]}
                       position={[position.x, 1.1, position.z]} // Slightly offset vertically
                       rotation={[0, torusRotation * (i % 2 === 0 ? -1 : 1), 0]} // Opposite rotation
                     >
-                      <meshStandardMaterial />
+                      <meshStandardMaterial
+                        color={color}
+                        roughness={0.5}
+                        metalness={0.5}
+                        opacity={0.5}
+                      />
                     </Torus>
                   </mesh>
                 </group>
               ))}
-              <fog attach="fog" args={[color, 2, 10]} />
-              <directionalLight
-                position={[10, 10, 10]}
-                shadow-mapSize-width={1024}
-                shadow-mapSize-height={1024}
-                intensity={11.5}
-              />
               <OrbitControls autoRotate />
-              <spotLight name="spotlight" position={[0, 10, 0]} intensity={20} />
+              <fog attach="fog" args={[color, 2, 10]} />
+              <mesh receiveShadow>
+                <boxGeometry args={[20, 0, 20]} />
+                <meshStandardMaterial color={'transparent'} />
+              </mesh>
             </scene>
           </Physics>
         </Suspense>
