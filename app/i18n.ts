75e2d10f03660e1d3619b82461ccba0c0b41675d
Lucas Pagotto Tonussi <ltonussi@deitres.com>
2025-08-05 10:42:40 -0300

    Default

--- app/i18n.ts
+++ app/i18n.ts
@@ -1,26 +1,24 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import enTranslations from "./locales/en-US/translation.json";
-import ptTranslations from "./locales/pt-BR/translation.json";
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import enTranslations from './locales/en-US/translation.json'
+import ptTranslations from './locales/pt-BR/translation.json'
 
 const resources = {
-  "en-US": {
-    translation: enTranslations
+  'en-US': {
+    translation: enTranslations,
   },
-  "pt-BR": {
-    translation: ptTranslations
-  }
-};
+  'pt-BR': {
+    translation: ptTranslations,
+  },
+}
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng: "pt-BR",
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en-US',
 
-    interpolation: {
-      escapeValue: false // react already safes from xss
-    }
-  });
+  interpolation: {
+    escapeValue: false, // react already safes from xss
+  },
+})
 
-  export default i18n;
+export default i18n
